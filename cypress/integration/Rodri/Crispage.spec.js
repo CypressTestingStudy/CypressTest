56986a345c7313c16058c560814a5d97cf431625
Rodrigo Andres Alvarez Maya <rodrigo.alvarez@CO-IT02120.local>
2020-08-07 13:48:56 -0500

    Second case succes on cris page

---
+++ cypress/integration/Rodri/Crispage.spec.js
@@ -0,0 +1,23 @@
+// type definitions for Cypress object "cy"
+/// <reference types="Cypress" />
+
+describe('My first test suit', () =>{
+
+    it('My first test case', () => {
+        //visit page
+        cy.visit('https://www.rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('.search-keyword').type('ap')
+        cy.wait(3000)
+        
+        cy.get('.products > :nth-child(2) > .product-name').should('have.text','Apple - 1 Kg')
+        cy.get(':nth-child(2) > .stepper-input > .increment').click();
+        cy.get(':nth-child(2) > .product-action > button').click();
+        cy.get(':nth-child(3) > .product-name').should('have.text','Grapes - 1 Kg')
+        cy.get(':nth-child(3) > .product-action > button').click();
+        cy.get(':nth-child(1) > :nth-child(3) > strong').should('have.text','2')
+        cy.get(':nth-child(2) > :nth-child(3) > strong').should('have.text','264') //hay un bug deberia ser 204
+
+    })
+
+
+})
