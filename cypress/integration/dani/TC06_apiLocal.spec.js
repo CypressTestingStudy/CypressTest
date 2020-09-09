3f49ee0c7d5805ab965960c10ef75532fed71c57
Daniel Fuentes <giracros@gmail.com>
2020-09-09 00:01:35 -0500

    Delete y Put ok

--- cypress/integration/dani/TC06_apiLocal.spec.js
+++ cypress/integration/dani/TC06_apiLocal.spec.js
@@ -11,26 +11,26 @@ describe('API Testing LocalEnv', () => {
 
   it('TC02 - POST - Crear un Item', () => {
     cy
-    .request('POST', '/posts', { title: 'Papuchito' , author: 'Henry'})
+    .request('POST', '/posts', { title: 'Marraneitor' , author: 'DanielFuentes'})
     .then((response) => {
     // response.body is automatically serialized into JSON
-    expect(response.body).to.have.property('title', 'Papuchito'); // true
+    expect(response.body).to.have.property('title', 'Marraneitor'); // true
   });
   });
 
 
   it('TC03 - PUT - Actualizar un Item de la listaw ', () => {
     cy
-    .request('PUT', '/posts', { title: 'Papuchito' , author: 'Henry'})
+    .request('PUT', '/posts/15', { title: 'Capuchon' , author: 'Jordi'})
     .then((response) => {
     // response.body is automatically serialized into JSON
-    expect(response.body).to.have.property('title', 'Papuchito'); // true
+    expect(response.body).to.have.property('title', 'Capuchon'); // true
   });
   });
 
   it('TC04 - DELETE - Borrar un item de la lista', () => {
     cy
-    .request('DELETE', '/posts/3')
+    .request('DELETE', '/posts/14')
     .then((response) => {
     // response.body is automatically serialized into JSON
     expect(response.body).to.have.empty;
