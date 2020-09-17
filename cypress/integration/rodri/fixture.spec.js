b4d4f8b5758fb1701d0e272964ba7fe25ecf3105
Rodrigo Andres Alvarez Maya <rodrigo.alvarez@CO-IT02120.local>
2020-09-17 11:38:35 -0500

    este si es el ultimo, toco ensayo error

--- cypress/integration/rodri/fixture.spec.js
+++ cypress/integration/rodri/fixture.spec.js
@@ -5,12 +5,12 @@
         it('My first fixture', () => {
             cy.visit('https://demo.nopcommerce.com/login')
             cy.wait(2000)
-            cy.fixture('Login').then((login)=>{  
+            /* cy.fixture('Login').then((login)=>{  
             cy.get('#Email').type(login.user)
             cy.get('#Password').type(login.password)
                 
             })
-            cy.get('form > .buttons > .button-1').click()
+            cy.get('form > .buttons > .button-1').click() */
                
             })
         }) 
