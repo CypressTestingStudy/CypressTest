41abaeb32db73187e8b4ef33eb1536990c8cbf91
Rodrigo Andres Alvarez Maya <rodrigo.alvarez@CO-IT02120.local>
2020-09-17 11:31:19 -0500

    9999

--- cypress/integration/rodri/CNweb.spec.js
+++ cypress/integration/rodri/CNweb.spec.js
@@ -13,29 +13,7 @@ import {
     }) */
     it('videos up next & more videos', () => {
     automation.navigator();
-    automation.OT().click()
-    automation.videoslander()
-    .click()
-    cy.wait(10000)
     
-    automation.selectvideo()
-    .click() 
-    cy.wait(120000)
 
-    /*automation.checkingprerollad()
-    .should('ADVERTISEMENT');
-    cy.wait(100000)*/
-
-    automation.pause()
-    .click()
-    cy.wait(3000)
-    
-    automation.play()
-    .click()
-    cy.wait(3000)
-
-    automation.unmute()
-    .click()
-
-    });
+    }); 
     });
--- cypress/integration/rodri/recorderplugingonChrome.spec.js
+++ cypress/integration/rodri/recorderplugingonChrome.spec.js
@@ -6,21 +6,9 @@ describe('test_name', function() {
    
        cy.visit('https://www.cartoonnetwork.co.uk/videos')
     
-       cy.viewport(1680, 922)
-    
-       cy.visit('https://www.cartoonnetwork.co.uk/videos')
-    
-       cy.get('div > .swiper-container > .swiper-wrapper > .swiper-slide:nth-child(4) > .background-image').click()
-    
-       cy.get('#bmpui-id-198 > .bmpui-container-wrapper > #bmpui-id-197 > .bmpui-container-wrapper > #bmpui-id-188').click()
-    
-       cy.get('#bmpui-id-198 > .bmpui-container-wrapper > #bmpui-id-197 > .bmpui-container-wrapper > #bmpui-id-187').click()
-    
-       cy.get('#bmpui-id-198 > .bmpui-container-wrapper > #bmpui-id-197 > .bmpui-container-wrapper > #bmpui-id-187').click()
-    
-       cy.get('.top-navigation > .primary > .links > .games > img').click()
+      
     
     })
    
-   })
+   }) 
    
